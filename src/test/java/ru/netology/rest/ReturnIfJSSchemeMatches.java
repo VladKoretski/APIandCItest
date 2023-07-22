b6d6f2e0fea7b69b2cbc10a42d91fc3a27e54aef
Vladko1973 <vpk1973@yandex.ru>
2023-07-22 15:46:41 +0300

    third commit

--- src/test/java/ru/netology/rest/ReturnIfJSSchemeMatches.java
+++ src/test/java/ru/netology/rest/ReturnIfJSSchemeMatches.java
@@ -5,20 +5,21 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
-public class ReturnIfJSSchemeMatches {    @Test
-void shouldReturnDemoAccounts() {
-    // Given - When - Then
-    // Предусловия
-    given()
-            .baseUri("http://localhost:9999/api/v1")
-            // Выполняемые действия
-            .when()
-            .get("/demo/accounts")
-            // Проверка соответствия переменной схеме
-            .then()
-            .statusCode(200)
-            // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
-            .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-    ;
-}
+public class ReturnIfJSSchemeMatches {
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверка соответствия Json-переменной схеме
+                .then()
+                .statusCode(200)
+                // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+        ;
+    }
 }
